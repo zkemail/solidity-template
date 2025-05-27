7e27810ffdd003f7ce8bddaca1685048369af393
Bence Haromi <56651250+benceharomi@users.noreply.github.com>
2025-05-27 10:38:12 +0100

    chore: bump solc to 0.8.30 and evm_version to prague

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
+pragma solidity ^0.8.30;
 
 import { Script } from "forge-std/Script.sol";
 import { Counter } from "../src/Counter.sol";
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
+pragma solidity ^0.8.30;
 
 import { Test } from "forge-std/Test.sol";
 import { Counter } from "../src/Counter.sol";
