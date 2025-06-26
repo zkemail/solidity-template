288e6756ffc7e407eeda7ece806f1f5fb710acca
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-06-26 11:57:06 +0100

    ci: testing scenario (fail comment updated with fail comment (with different %))

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -8,18 +8,19 @@ contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        // counter = new Counter();
+        // counter.setNumber(0);
     }
 
     // Just for testing
-    // function test_Increment() public {
-    //     counter.increment();
-    //     assertEq(counter.number(), 1);
-    // }
+    function test_Increment() public {
+        // counter.increment();
+        // assertEq(counter.number(), 1);
+    }
 
+    // Just for testing
     function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        // counter.setNumber(x);
+        // assertEq(counter.number(), x);
     }
 }
